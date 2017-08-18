7eac21eb5077818512a30ff5c3f01d57fb1cefff
shajithkadachi <shajit_k@yahoo.com>
2017-08-19 08:03:41 +1000

    Adding web project

---
+++ restful-web/src/main/java/com/io/shajith/App.java
@@ -0,0 +1,13 @@
+package com.io.shajith;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
